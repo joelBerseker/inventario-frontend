749126ed1840b4e543f99d4076f13fa205e92053
Leowi <jhoshep_1999@hotmail.com>
2023-06-15 23:23:38 -0500

    addmodal

--- src/views/Product.vue
+++ src/views/Product.vue
@@ -1,9 +1,50 @@
+<script setup lang="ts">
+import AddProduct from '../components/AddProduct.vue'
+</script>
 <template>
-  <div>
-    <h1>Productos</h1>
-  </div>
+    <!-- Modal -->
+    <AddProduct/>
+    <div>
+        <h1>Productos</h1>
+
+        <button type="button" class="btn btn-dark mb-2" data-bs-toggle="modal" data-bs-target="#exampleModal">Agregar</button>
+        <table class="table">
+            <thead class="table-dark">
+                <tr>
+                    <td>ID</td>
+                    <td>Nombre</td>
+                    <td>Cantidad</td>
+                    <td>Ultimo ingresado</td>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>Prod1</td>
+                    <td>1</td>
+                    <td>10-09-05</td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>Prod1</td>
+                    <td>1</td>
+                    <td>10-09-05</td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>Prod1</td>
+                    <td>1</td>
+                    <td>10-09-05</td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>Prod1</td>
+                    <td>1</td>
+                    <td>10-09-05</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 </template>
 
-<style>
-
-</style>
+<style></style>
