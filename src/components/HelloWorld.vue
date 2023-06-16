065e683b94f02de893731aad0e0760a220fc5268
joelBersek <jatamaria@unsa.edu.pe>
2023-06-15 19:33:32 -0500

    edit HelloWorld

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -8,7 +8,7 @@ defineProps<{
   <div class="greetings">
     <h1 class="green">{{ msg }}</h1>
     <h3>
-      INVENTARIOS
+      INVENTARIOS nuevo
       <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
       <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next?
     </h3>
