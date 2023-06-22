38f34a8cabd192d1b0ddd7d13a7aa17e4e4b62a1
Leowi <jhoshep_1999@hotmail.com>
2023-06-21 23:39:47 -0500

    change table from product - home functional

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -1,35 +1,65 @@
-<template> 
+<template>
     <MainContent :title="'Inicio'" :icon="'bi bi-house'">
         <div class="row">
-            <div class="col-6">
-                <div class="card mb-3">
-                    <div class="card-body">
-                        <p class="title-text"><i class="bi bi-box-seam"></i> Productos</p>
-                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-6">
-                <div class="card">
+            <div class="col-6 mb-3" v-for="item in list" :key="item.title" v-on:click="goTo(item.url)">
+                <div class="card box" :id="key">
 
                     <div class="card-body">
-                        <h5 class="title-text"><i class="bi bi-people"></i> Clientes</h5>
-                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-6">
-                <div class="card">
-
-                    <div class="card-body">
-                        <p class="title-text"><i class="bi bi-truck"></i> Proveedores</p>
-                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                        <p class="title-text"><i :class="item.icon"></i> {{ item.title }}</p>
+                        <p class="card-text">{{ item.desc }}</p>
                     </div>
                 </div>
             </div>
         </div>
     </MainContent>
 </template>
-<script setup>
+<style scoped>
+.box{
+    cursor: pointer;
+}
+.box:hover{
+    background-color: rgb(224, 224, 224);
+}
+</style>
+<script>
 import MainContent from '@/components/my_components/MainContent.vue'
+import { defineComponent } from "vue";
+export default defineComponent({
+    name: "Home",
+    props: [
+        "title", "icon"
+    ],
+    components: {
+        MainContent
+    },
+    data() {
+        return {
+            list: [
+                {
+                    title: "Productos",
+                    desc: "With supporting text below as a natural lead-in to additional content.",
+                    icon: "bi bi-box-seam",
+                    url: "/product"
+                },
+                {
+                    title: "Clientes",
+                    desc: "With supporting text below as a natural lead-in to additional content.",
+                    icon: "bi bi-people",
+                    url: "/supplier"
+                },
+                {
+                    title: "Proveedores",
+                    desc: "With supporting text below as a natural lead-in to additional content.",
+                    icon: "bi bi-truck",
+                    url: "/customer"
+                }
+            ]
+        }
+    },
+    methods: {
+        goTo(url) {
+            this.$router.push(url);
+        }
+    }
+});
 </script>
